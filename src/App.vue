546503a191ba34474e33eb6585cd5db4f33edc0f
King Akidi <onlinemraam@gmail.com>
2022-06-26 17:58:23 +0100

    first commitn on ant design

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,71 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h2>Welcome to Ant Design</h2>
+    <div v-for="post in posts" :key="post.id">
+      {{ post.id }} - {{ post.title }} - {{ post.author }}
+    </div>
+    <a-pagination
+      v-model:current="current"
+      :total="totalPost"
+      @change="pageClick"
+    />
+    <a-row>
+      <a-col :span="6" class="bg-color">
+        {{ current }}
+      </a-col>
+      <a-col :span="6">col - 6</a-col>
+
+      <a-col :span="6">col - 6</a-col>
+    </a-row>
+    <a-button :type="primary" @click="getBottonType">Primary Button</a-button>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from "axios";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      primary: "dashed",
+      current: 1,
+      posts: [],
+      postLength: 0,
+    };
+  },
+  computed: {
+    totalPost() {
+      return this.postLength;
+    },
+  },
+  methods: {
+    async pageClick(page) {
+      await axios
+        .get(`http://localhost:3000/posts?_page=${page}`)
+        .then((res) => {
+          this.posts = res.data;
+        });
+    },
+    getBottonType(e) {
+      console.log(e);
+    },
+    async getFirstPost() {
+      await axios.get("http://localhost:3000/posts?_page=1").then((res) => {
+        this.posts = res.data;
+        // console.log(res);
+        // console.log(this.posts.length);
+      });
+
+      await axios.get("http://localhost:3000/posts").then((res) => {
+        this.postLength = res.data.length;
+      });
+    },
+  },
+  mounted() {
+    this.getFirstPost();
+  },
+};
 </script>
 
 <style>
@@ -22,5 +76,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.bg-color {
+  background: rgb(0, 0, blue);
 }
 </style>
